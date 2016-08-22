345742dff81a0def3b4447371ee48b2ed2214570
Ryota Uchiyama <lessironglance@gmail.com>
2016-08-22 22:58:29 +0900

    add series

---
+++ Chaert/Chaert/Series.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chart
+{
+    class Series
+    {
+        DataPointCollectoin Points;
+
+        double Max_X;
+        double Max_Y;
+
+    }
+}
